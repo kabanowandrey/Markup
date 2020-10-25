cf664e90de93b76094c7fb9ca73b7ed5a6a1a43e
Andry <kabanowandrey@gmail.com>
2020-10-25 20:49:38 +0200

    only first sheet left

---
+++ script/script.js
@@ -0,0 +1,20 @@
+
+
+
+
+//-------------------------------------------Скрипт на сокрытие/обнаружение текста на странице История группы--------------
+document.onclick = function(event) {
+    var target = event.target;
+
+    var id = target.getAttribute('data-toggle-id');
+    if (!id) return;
+
+    var elem = document.getElementById(id);
+
+    elem.hidden = !elem.hidden;
+  };
+
+  //------------------------------------------------------------------------------------------------------------------
+  
+
+  

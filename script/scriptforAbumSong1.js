22f6cdbfcfb9f202d2e1f3ee84fae45c0c84dd98
Andry <kabanowandrey@gmail.com>
2020-10-26 10:20:08 +0200

    Finall project

--- script/scriptforAbumSong1.js
+++ script/scriptforAbumSong1.js
@@ -9,7 +9,7 @@ function audioPlayer() {
     $('#audioPlayer')[0].play();
     $('#playlist li a').click(function (e) {
         e.preventDefault();
-        $('#audioPlayer')[0].src =this;
+        $('#audioPlayer')[0].src = this;
         $('#audioPlayer')[0].play();
         $('#playlist li').removeClass('current-song');
             currentSong = $(this).parent().IronMaidenSongList();
@@ -21,7 +21,7 @@ function audioPlayer() {
         if (currentSong == $('#playlist li a').length)
             currentSong = 0;
         $('#playlist li').removeClass('current-song');
-        $('#playlist li:eq("+currentSong+")').addClass('current-song');
+        $('#playlist li:eq("+ currentSong +")').addClass('current-song');
         $('#audioPlayer')[0].src = $('#playlist li a')[currentSong].href;
         $('#audioPlayer')[0].play();
     });
